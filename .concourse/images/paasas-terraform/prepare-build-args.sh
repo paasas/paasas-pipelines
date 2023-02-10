8caef14185b58d3a680b4d05cbec61b2da36f1d1
Daniel Lavoie <dlavoie@live.ca>
2023-02-10 11:34:06 -0500

    fix(images): build-args for paasas-terraform

---
+++ .concourse/images/paasas-terraform/prepare-build-args.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TERRAFORM_VERSION=$(cat terraform-image/tag)
+
+read -r -d '' BUILD_ARGS << EOM
+{
+  "TERRAFORM_VERSION":  "${TERRAFORM_VERSION}"
+}
+EOM
+
+echo "$BUILD_ARGS" > build-args/build-args.json
