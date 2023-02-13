5fbdba260d323ed2f59d7c8d33cc103955d4bf6c
Daniel Lavoie <dlavoie@live.ca>
2023-02-13 10:07:40 -0500

    fix(ci): native compile

--- .concourse/tasks/build/build.sh
+++ .concourse/tasks/build/build.sh
@@ -5,7 +5,8 @@ set -x
 export JAVA_HOME=/root/.sdkman/candidates/java/current
 
 pushd src && \
-  ./mvnw -Pnative native:compile && \
+  ./mvnw install && \
+  ./mvwn -f concourse/pom.xml -Pnative native:compile && \
   ./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout > ../version/version && \
   popd && \
   mv src/concourse/target/pipelines-concourse build/pipelines-concourse
