ad41a452381db8a975314f1b4a6534e76f8fb3e7
Daniel Lavoie <dlavoie@live.ca>
2023-02-15 11:02:35 -0500

    chore(ci): switch gcs bucket to fx-demo-terraform-states

--- .concourse/tasks/generate-platform-pipelines/generate-platform-pipelines.sh
+++ .concourse/tasks/generate-platform-pipelines/generate-platform-pipelines.sh
@@ -5,7 +5,13 @@ if [ -z "${TARGETS_DIRECTORY}" ]; then
   exit 1
 fi
 
-paasas-pipelines \
+if [ -z "${TERRAFORM_BACKEND_GCS_BUCKET}" ]; then
+  echo "env variable TERRAFORM_BACKEND_GCS_BUCKET is undefined"
+  exit 1
+fi 
+
+PIPELINES_TERRAFORMBACKENDGCSBUCKET=${TERRAFORM_BACKEND_GCS_BUCKET} && \
+  paasas-pipelines \
     generate-pipeline \
     platforms-src/$TARGETS_DIRECTORY \
     pipelines/pipelines.yaml && \
