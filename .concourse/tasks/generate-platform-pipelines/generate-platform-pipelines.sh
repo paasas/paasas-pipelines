7fa1227ed8cc949520d86827e17eae2e2bc73645
Daniel Lavoie <dlavoie@live.ca>
2023-02-23 11:41:57 -0500

    debug(ci): arguments for generate pipeline

--- .concourse/tasks/generate-platform-pipelines/generate-platform-pipelines.sh
+++ .concourse/tasks/generate-platform-pipelines/generate-platform-pipelines.sh
@@ -12,10 +12,13 @@ fi
 
 PIPELINES_CONCOURSE_TERRAFORMBACKENDGCSBUCKET="${TERRAFORM_BACKEND_GCS_BUCKET}" \
   PIPELINES_CONCOURSE_TERRAFORMSRCBRANCH=main \
+  pipelines.concourse.platform-path-prefix=teams/$TARGETS_DIRECTORY \
+  set -x && \
   java -jar /opt/paasas-pipelines/paasas-pipelines.jar \
     generate-pipeline \
     platforms-src/$TARGETS_DIRECTORY \
     pipelines/pipelines.yaml && \
+  set +x && \
   echo "Generated pipeline:"
   echo ""
   echo "$(cat pipelines/pipelines.yaml)"
