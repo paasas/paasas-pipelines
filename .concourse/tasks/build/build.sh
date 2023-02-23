da4ee637398d007e5123a96f72dca16d2eb251e7
Daniel Lavoie <dlavoie@live.ca>
2023-02-23 08:40:15 -0500

    fix(ci): remove graalvm compilation

--- .concourse/tasks/build/build.sh
+++ .concourse/tasks/build/build.sh
@@ -5,7 +5,7 @@ set -x
 export JAVA_HOME=/root/.sdkman/candidates/java/current
 
 pushd src && \
-  ./mvnw -Pnative native:compile && \
+  ./mvnw install && \
   ./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout > ../version/version && \
   popd && \
   mv src/target/paasas-pipelines build/paasas-pipelines
