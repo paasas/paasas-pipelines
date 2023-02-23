31f53ad4bfb70bf4a74f1fec65c72a7739eb73ac
Daniel Lavoie <dlavoie@live.ca>
2023-02-23 08:46:11 -0500

    chore(ci): add maven caching

--- .concourse/tasks/build/build.sh
+++ .concourse/tasks/build/build.sh
@@ -2,9 +2,15 @@
 
 set -x
 
+export M2_HOME=~/.m2
+
+mkdir -p ${M2_HOME}
+
 export JAVA_HOME=/root/.sdkman/candidates/java/current
 
 pushd src && \
+  rm -rf ~/.m2 && \
+  ln -fs $(pwd)/m2 ~/.m2 && \
   ./mvnw install && \
   ./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout > ../version/version && \
   popd && \
