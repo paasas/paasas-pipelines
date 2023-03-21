c04b0898e84d8010052e72d02fb66c5e707517e1
daniellavoie <dlavoie@live.ca>
2023-03-21 12:30:11 -0400

    fix(ci): invalid auth check for cloudrun-deploy task

--- .concourse/tasks/cloudrun/cloudrun-deploy.sh
+++ .concourse/tasks/cloudrun/cloudrun-deploy.sh
@@ -10,10 +10,6 @@ if [ -z "${PIPELINES_CLOUDRUN_GOOGLECREDENTIALSJSON}" ]; then
   echo "env variable PIPELINES_CLOUDRUN_GOOGLECREDENTIALSJSON is required"
   exit 1
 fi
-  
-if [ ${AUTH_RESULT} -ne 0 ]; then
-  exit 1
-fi
 
 PIPELINES_CONCOURSE_DEPLOYMENTPATHPREFIX=$MANIFEST_PATH && \
   java -jar /opt/paasas-pipelines/paasas-pipelines.jar \
