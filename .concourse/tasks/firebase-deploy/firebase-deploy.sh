dc75996bb12e7d5c059287d5a292a0e9d934fa61
daniellavoie <dlavoie@live.ca>
2023-08-30 11:06:48 +0200

    feat: register firebase deployment

--- .concourse/tasks/firebase-deploy/firebase-deploy.sh
+++ .concourse/tasks/firebase-deploy/firebase-deploy.sh
@@ -16,9 +16,16 @@ if [ -z "${GCP_PROJECT_ID}" ]; then
   exit 1
 fi
 
+if [ -z "${MANIFEST_PATH}" ]; then
+  echo "Env variable MANIFEST_PATH is undefined"
+  exit 1
+fi
+
 if [  "${GOOGLE_IMPERSONATE_SERVICE_ACCOUNT}" != "" ]; then
   GCLOUD_FLAGS="--impersonate-service-account=$GOOGLE_IMPERSONATE_SERVICE_ACCOUNT"
 fi
+
+WORKDIR=$(pwd)
 
 mkdir -p /root/.config/gcloud
 
@@ -38,7 +45,6 @@ if [ ! -z "${FIREBASE_CONFIG}" ]; then
 fi
 
 echo "$GOOGLE_CREDENTIALS" > /root/.config/gcloud/application_default_credentials.json && \
-  set -x && \
   gcloud auth activate-service-account ${GCLOUD_FLAGS} --key-file=/root/.config/gcloud/application_default_credentials.json && \
   pushd src/${FIREBASE_APP_PATH} && \
   echo "$FIREBASERC" > .firebaserc && \
@@ -46,3 +52,66 @@ echo "$GOOGLE_CREDENTIALS" > /root/.config/gcloud/application_default_credential
       --token $(gcloud auth print-access-token $GCLOUD_FLAGS) \
     deploy \
       --only hosting
+
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+set -x
+
+if [ ! -z "$PIPELINES_SERVER" ]; then
+  cd ${WORKDIR}/src && \
+  COMMIT=$(git rev-parse HEAD) && \
+  TAG=$(/usr/local/bin/yq -o=json -I=0 ".firebaseApp.git.tag" ${WORKDIR}/manifest-src/${MANIFEST_PATH}) && \
+  PATH=$(/usr/local/bin/yq -o=json -I=0 ".firebaseApp.git.path" ${WORKDIR}/manifest-src/${MANIFEST_PATH}) && \
+  NPM=$(/usr/local/bin/yq -o=json -I=0 ".firebaseApp.npm" ${WORKDIR}/manifest-src/${MANIFEST_PATH}) && \
+  OPTIONAL_TAG="" && \
+  OPTIONAL_PATH=""
+  
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
+  
+  if [ "$TAG" != "null" ]; then
+    OPTIONAL_TAG="\n,    \"tag\": ${TAG}"
+  fi
+  
+  if [ "$PATH" != "null" ]; then
+    OPTIONAL_PATH=", \"path\": $PATH"
+  fi
+  
+  BUILD=$(/bin/cat ${WORKDIR}/build-metadata/build-name) && \
+  JOB=$(/bin/cat ${WORKDIR}/build-metadata/build-job-name) && \
+  PIPELINE=$(/bin/cat ${WORKDIR}/build-metadata/build-pipeline-name) && \
+  TEAM=$(/bin/cat ${WORKDIR}/build-metadata/build-team-name) && \
+  URL=$(/bin/cat ${WORKDIR}/build-metadata/atc-external-url) && \
+  COMMIT_AUTHOR=$(/bin/cat ${WORKDIR}/src/.git/committer) && \
+  read -r -d '' FIREBASE_APP_DEPLOYMENT << EOM
+{
+  "jobInfo": {
+    "build": "$BUILD",
+    "job": "$JOB",
+    "pipeline": "$PIPELINE",
+    "projectId": "$GCP_PROJECT_ID",
+    "team": "$TEAM",
+    "url": "$URL"
+  },
+  "gitRevision": {
+    "commit": "${COMMIT}",
+    "commitAuthor": "$COMMIT_AUTHOR",
+    "repository": "${GITHUB_REPOSITORY}"${OPTIONAL_TAG}${OPTIONAL_PATH}
+  },
+  "npm": $NPM
+}
+EOM
+
+  if [ -z "$FIREBASE_APP_DEPLOYMENT" ]; then
+    exit 1
+  fi
+  
+  /usr/bin/curl -X POST $PIPELINES_SERVER/api/ci/deployment/firebase \
+    --fail \
+    -H "Content-Type: application/json" \
+    -u $PIPELINES_SERVER_USERNAME:$PIPELINES_SERVER_PASSWORD \
+    -d "$FIREBASE_APP_DEPLOYMENT"
+fi
