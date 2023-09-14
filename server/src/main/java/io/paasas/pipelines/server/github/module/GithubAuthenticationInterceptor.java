85ca4fa5e9172a97132a197208897a4ee4204eb9
daniellavoie <dlavoie@live.ca>
2023-09-14 10:31:08 -0400

    fix: renew github access token if expired

--- server/src/main/java/io/paasas/pipelines/server/github/module/GithubAuthenticationInterceptor.java
+++ server/src/main/java/io/paasas/pipelines/server/github/module/GithubAuthenticationInterceptor.java
@@ -6,6 +6,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.time.LocalDateTime;
 import java.util.Base64;
 import java.util.Date;
 import java.util.List;
@@ -25,12 +26,15 @@ import org.springframework.web.client.RestTemplate;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class GithubAuthenticationInterceptor implements ClientHttpRequestInterceptor {
 	private final GithubConfiguration githubConfiguration;
 	private final RestTemplate restTemplate;
 
 	private GithubAccessTokenResponse accessToken;
+	private LocalDateTime tokenExpiration;
 
 	public GithubAuthenticationInterceptor(GithubConfiguration githubConfiguration) {
 		this.githubConfiguration = githubConfiguration;
@@ -41,18 +45,29 @@ public class GithubAuthenticationInterceptor implements ClientHttpRequestInterce
 	}
 
 	synchronized GithubAccessTokenResponse accessToken() {
+		if(tokenExpiration != null && tokenExpiration.isBefore(LocalDateTime.now()) ) {
+			accessToken = null;
+		}
+		
 		if (accessToken == null) {
-			this.accessToken = restTemplate.exchange(
-					"/app/installations/{installationId}/access_tokens",
-					HttpMethod.POST,
-					new HttpEntity<Void>(
-							new HttpHeaders(new MultiValueMapAdapter<>(Map.of(
-									HttpHeaders.ACCEPT, List.of("application/vnd.github+json"),
-									HttpHeaders.AUTHORIZATION, List.of("Bearer " + generateJwt()),
-									"X-GitHub-Api-Version", List.of(githubConfiguration.getApiVersion()))))),
-					GithubAccessTokenResponse.class,
-					githubConfiguration.getInstallationId())
+			log.info("Renewing Github access token");
+			
+			tokenExpiration = LocalDateTime.now().plusHours(1).minusMinutes(5);
+			
+			this.accessToken = restTemplate
+					.exchange(
+							"/app/installations/{installationId}/access_tokens",
+							HttpMethod.POST,
+							new HttpEntity<Void>(
+									new HttpHeaders(new MultiValueMapAdapter<>(Map.of(
+											HttpHeaders.ACCEPT, List.of("application/vnd.github+json"),
+											HttpHeaders.AUTHORIZATION, List.of("Bearer " + generateJwt()),
+											"X-GitHub-Api-Version", List.of(githubConfiguration.getApiVersion()))))),
+							GithubAccessTokenResponse.class,
+							githubConfiguration.getInstallationId())
 					.getBody();
+			
+			log.info("Successfully renewed Github access token");
 		}
 		return accessToken;
 	}
